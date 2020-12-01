f2c36011f0b9f6894f32cdb1ac363c9a8c589d18
시혁 박 <exon.indiv@gmail.com>
2020-12-01 22:15:04 +0900

    preview

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 function main() {
-    setTimeout(get, 6000)
+    setTimeout(find, 6000)
 }
 
 async function getStar(name) {
@@ -11,22 +11,36 @@ async function getStar(name) {
 
 function find() {
     try {
-        const profiles = document.querySelectorAll('#dashboard > div > div > div > div > div > span')
-        profiles.forEach((profile) => {
-            profile.addEventListener("mouseover", () => {
-                setTimeout(showStar, 1000)
-            })
-        })
-    } catch (err){
+        const profiles1 = document.querySelectorAll('#dashboard > div > div > div > div > div > span')
+        const profiles2 = document.querySelectorAll('#dashboard > div > div > div > div > div > div > div.d-flex.flex-items-baseline > div > a.link-gray-dark.no-underline.text-bold.wb-break-all.d-inline-block')
+        addEventMouseEnter(profiles1)
+        addEventMouseEnter(profiles2)
+    } catch (err) {
         console.log(err)
     }
 }
 
+function addEventMouseEnter(arr) {
+    arr.forEach((profile) => {
+        profile.addEventListener("mouseenter", () => {
+            console.log("enter")
+            setTimeout(showStar, 1000)
+        })
+    })
+}
+
 async function showStar() {
-    const userInfo = document.querySelector('body > div.Popover.js-hovercard-content.position-absolute > div > div > div > div.d-flex.mt-3 > div.overflow-hidden.ml-3 > div.mt-1 > div')
-    const userName = document.querySelector('body > div.Popover.js-hovercard-content.position-absolute > div > div > div > div.d-flex.mt-3 > div.overflow-hidden.ml-3 > a.link-gray.no-underline.ml-1')
-    const stars = await getStar(userName.innerHTML)
-    userInfo.innerHTML += '\nStar: ' + stars
+    try {
+        const userInfo = document.querySelector('body > div.Popover.js-hovercard-content.position-absolute > div > div > div > div.d-flex.mt-3 > div.overflow-hidden.ml-3 > div.mt-1 > div')
+        const userName = document.querySelector('body > div.Popover.js-hovercard-content.position-absolute > div > div > div > div.d-flex.mt-3 > div.overflow-hidden.ml-3 > a.link-gray.no-underline.ml-1')
+        const stars = await getStar(userName.innerHTML)
+        userInfo.innerHTML += '\n<b>⭐: ' + stars + '</b>'
+    } catch (err) {
+        console.log(err)
+    }
+
 }
 
 main()
+
+// body > div.Popover.js-hovercard-content.position-absolute > div > div > div > div.f6.border-bottom.mr-n3.ml-n3.user-status-container
